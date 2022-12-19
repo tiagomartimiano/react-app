aee3d34a85f6877bee56d05a41937b1e1a7fa1f3
tiagomartimiano <tiago_ccomp@uninove.edu.br>
2022-12-19 10:07:28 -0300

    fix: include settings for assets in the release.config.js file

--- release.config.js
+++ release.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-		branches: "master",
-    repositoryURL: "https://github.com/tiagomartimiano/react-app.git"
-    plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator', 
-        '@semantic-release/github']	
-}
+  branches: "master",
+  repositoryURL: "https://github.com/alanbrunoscience/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
+};
