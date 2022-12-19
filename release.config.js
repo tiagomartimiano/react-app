82d7a67d303fa0b7b1061a4d7953fc4842fdbf0d
tiagomartimiano <tiago_ccomp@uninove.edu.br>
2022-12-18 23:28:03 -0300

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+		branches: "master",
+    repositoryURL: "https://github.com/tiagomartimiano/react-app.git"
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github']	
+}
